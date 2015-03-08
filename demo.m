ff3407cf3496afff6424d4389511bdd32a2bf1cd
Yaliang Wang <yaliangwang2015@u.northwestern.edu>
2015-03-08 12:09:48 -0500

    initial commit

---
+++ demo.m
@@ -0,0 +1,130 @@
+function varargout = demo(varargin)
+% DEMO MATLAB code for demo.fig
+%      DEMO, by itself, creates a new DEMO or raises the existing
+%      singleton*.
+%
+%      H = DEMO returns the handle to a new DEMO or the handle to
+%      the existing singleton*.
+%
+%      DEMO('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in DEMO.M with the given input arguments.
+%
+%      DEMO('Property','Value',...) creates a new DEMO or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before demo_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to demo_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help demo
+
+% Last Modified by GUIDE v2.5 06-Mar-2015 16:05:53
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @demo_OpeningFcn, ...
+                   'gui_OutputFcn',  @demo_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before demo is made visible.
+function demo_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to demo (see VARARGIN)
+
+% Choose default command line output for demo
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes demo wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+read_data(handles);
+axes(handles.axes1);
+axis off;
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = demo_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on selection change in listbox1.
+function listbox1_Callback(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns listbox1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from listbox1
+global s
+contents = cellstr(get(hObject,'String'));
+value = contents{get(hObject,'Value')};
+value = strsplit(value,'_');
+select = num2str(str2num(value{2}));
+axes(handles.axes1);
+imshow(s.(strcat('image_',select)));
+set(handles.text1,'String',num2str(s.(strcat('label_',select))));
+
+
+% --- Executes during object creation, after setting all properties.
+function listbox1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+function read_data(handles) 
+global s
+% read images
+rb = fileread('train-images.idx3-ubyte');
+imageNum = 100;
+for i=1:imageNum
+    img = uint8(rb((16+28*28*(i-1)+1):(16+28*28*(i))));
+    img = vec2mat(img,28);
+    s.(strcat('image_',num2str(i))) = img;
+end
+% read labels
+rb = fileread('train-labels.idx1-ubyte');
+for i=1:imageNum
+    label = uint8(rb(8+i));
+    s.(strcat('label_',num2str(i))) = label;
+end
+list = {};
+for i=1:imageNum
+    list = [list {strcat('Sample_', num2str(i))}];
+end
+set(handles.listbox1,'String',list);
---
+++ nn_bp_gpu.m
@@ -0,0 +1,61 @@
+% BP neural network
+
+% initialize
+hu = 1000;
+dim = 28*28;
+imageNum = 60000;
+nn.hl_w = (rand(hu,dim)-0.5)*0.01;
+nn.hl_b = (rand(hu,1)-0.5)*0.01;
+nn.ol_w = (rand(10,hu)-0.5)*0.01;
+nn.ol_b = (rand(10,1)-0.5)*0.01;
+iter = 1000;
+alpha = 8.9*(10^-3);
+lambda = 2.4*1/imageNum;
+ 
+% read images
+input = zeros(28*28,imageNum);
+target = zeros(10,imageNum);
+truth = zeros(1,imageNum);
+rb = fileread('train-images.idx3-ubyte');
+for i=1:imageNum
+    img = double(rb((16+28*28*(i-1)+1):(16+28*28*(i))));
+    input(1:28*28,i) = img;
+end
+% read labels
+rb = fileread('train-labels.idx1-ubyte');
+for i=1:imageNum
+    label = uint8(rb(8+i));
+    target(label+1,i) = 1;
+    truth(1,i) = label;
+end
+
+for k = 1:iter
+    % feedforward
+    nn.hl_z = nn.hl_w*input + repmat(nn.hl_b,1,imageNum);
+    nn.hl_a = 1./(1+exp(-nn.hl_z));
+    nn.ol_z = nn.ol_w*nn.hl_a + repmat(nn.ol_b,1,imageNum);
+    nn.ol_a = 1./(1+exp(-nn.ol_z));
+    
+    % backpropagation
+    nn.ol_s = -(target - nn.ol_a).*(nn.ol_a.*(1-nn.ol_a));
+    nn.hl_s = (nn.ol_w'*nn.ol_s).*(nn.hl_a.*(1-nn.hl_a));
+    nn.ol_d_w = nn.ol_s * nn.hl_a';
+    nn.ol_d_b = sum(nn.ol_s,2);
+    nn.hl_d_w = nn.hl_s * input';
+    nn.hl_d_b = sum(nn.hl_s,2);
+
+    % update
+    nn.hl_w = nn.hl_w - alpha * ((1/imageNum)*nn.hl_d_w + lambda*nn.hl_w);
+    nn.hl_b = nn.hl_b - alpha * ((1/imageNum)*nn.hl_d_b);
+    nn.ol_w = nn.ol_w - alpha * ((1/imageNum)*nn.ol_d_w + lambda*nn.ol_w);
+    nn.ol_b = nn.ol_b - alpha * ((1/imageNum)*nn.ol_d_b);
+    
+    % calculate correct number
+    [~,nn.result] = max(nn.ol_a,[],1);
+    nn.result = nn.result-1;
+    error = sum(sign(abs(nn.result-truth)));
+    correct = imageNum - error;
+    
+    % display
+    display(strcat('Iteration ',num2str(k),':',num2str(correct),'/',num2str(imageNum)));
+end
